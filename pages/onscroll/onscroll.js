bef8b7b472b200e79f3ccc94779d95502755c978
Mar-cloud <13930333639@163.com>
2020-02-16 21:52:08 +0800

    小程序内置样式/公共样式引入

--- pages/onscroll/onscroll.js
+++ pages/onscroll/onscroll.js
@@ -1,9 +1,58 @@
 // pages/onscroll/onscroll.js
+// var base64 = require("../images/base64");
+function _next () {
+  var that = this;
+  if (this.data.progress >= 100) {
+    this.setData({
+      disabled: false
+    });
+    return true;
+  }
+  this.setData({
+    progress: ++this.data.progress
+  });
+  setTimeout(function () {
+    _next.call(that);
+  }, 20);
+}
 Page({
   data: {
+    inputShowed: false,
+    inputVal: "",
+    progress: 0,
+    disabled: false
+  },
+  upload: function () {
+    if (this.data.disabled) return;
 
+    this.setData({
+      progress: 0,
+      disabled: true
+    });
+    _next.call(this);
   },
   getScoll (event) {
     console.log(event)
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
 })
